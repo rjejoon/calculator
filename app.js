ca6912e3bd8603d30dcf4a01de0f8a7eb3bd478b
Jejoon Ryu <rjejoon@gmail.com>
2021-06-11 18:40:10 -0600

    Add three unary operations

--- app.js
+++ app.js
@@ -20,55 +20,6 @@ function btnMouseDownHandler(e) {
     this.classList.add('onclick');
 
     btnAction(this);
-
-}
-
-function btnAction(btn) {
-
-    const n = output.textContent;
-    const val = btn.dataset.value;
-
-    // TODO check whether it's valid
-    if (btn.classList.contains("clear")) {
-        resetCalc();
-    }
-    else if (btn.classList.contains("operator")) {
-        if (output.textContent.length > 0) {
-            inputs.push((n.indexOf('.') > -1) ? parseFloat(n) : parseInt(n));
-            output.textContent = "";
-        }
-        inputs.push(val);
-        eq.textContent += " " + val + " ";
-
-        if (isDecimal) enableDecimalBtn();
-    }
-    else if (btn.classList.contains("bracket") && isValidBracket(val)) {
-        if (output.textContent.length > 0) {
-            inputs.push((n.indexOf('.') > -1) ? parseFloat(n) : parseInt(n));
-            output.textContent = "";
-        }
-        inputs.push(val);
-        eq.textContent += val;
-    }
-    else if (btn.classList.contains("decimal")) {
-        output.textContent += val;
-        eq.textContent += val;
-
-        if (!isDecimal) disableDecimalBtn();
-    }
-    else if (btn.classList.contains("number")) {
-        output.textContent += val;
-        eq.textContent += val;
-    }
-    else if (btn.classList.contains("equal")) {
-        // TODO evaluate
-        if (output.textContent.length > 0) {
-            inputs.push((n.indexOf('.') > -1) ? parseFloat(n) : parseInt(n));
-        }
-        output.textContent = operate();
-        inputs = [];
-    }
-
 }
 
 function btnMouseUpHandler(e) {
@@ -80,10 +31,110 @@ function btnMouseLeaveHandler(e) {
 }
 
 function keydownHandler(e) {
-    const targetBtn = document.querySelector(`.button-item[data-value="${e.key}"]`);
+    const targetBtn = document.querySelector(`.button-item[data-key="${e.key}"]`);
     if (!targetBtn) return;
 
     btnAction(targetBtn);
+}
+
+function btnAction(btn) {
+
+    const n = output.textContent;
+    const val = btn.dataset.value;
+
+    // TODO check whether it's valid
+    if (btn.classList.contains("clear")) {
+        resetCalc();
+
+    } else if (btn.classList.contains("delete")) {
+        output.textContent = output.textContent.slice(0, -1);
+
+    } else if (btn.classList.contains("operator")) {
+
+        if (btn.classList.contains("unary")) {
+            unaryOpAction(val, n);
+        } else {
+            binaryOpAction(val, n);
+        }
+        if (isDecimal) enableDecimalBtn();
+
+    } else if (btn.classList.contains("bracket") && isValidBracket(val)) {
+        if (output.textContent.length > 0) {
+            inputs.push((n.indexOf('.') > -1) ? parseFloat(n) : parseInt(n));
+            output.textContent = "";
+        }
+        inputs.push(val);
+        eq.textContent += val;
+
+    } else if (btn.classList.contains("decimal")) {
+        output.textContent += val;
+
+        if (!isDecimal) disableDecimalBtn();
+
+    } else if (btn.classList.contains("number")) {
+        output.textContent += val;
+
+    } else if (btn.classList.contains("equal")) {
+        // TODO evaluate
+        if (output.textContent.length > 0) {
+            inputs.push(numstrToNumber(n));
+        }
+        eq.textContent += `${n} =`;
+
+        const result = operate();
+        output.textContent = result;
+        inputs = [result];
+    }
+
+    if (output.textContent.length > 0) {
+        toggleClearBtn(document.querySelector('.clear'));
+    } else {
+        toggleDelBtn(document.querySelector('.delete'));
+    }
+}
+
+function numstrToNumber(numstr) {
+    if (numstr.length > 0) {
+        return (numstr.indexOf('.') > -1) ? parseFloat(numstr) : parseInt(numstr);
+    }
+    return null;
+}
+
+function toggleClearBtn(clearBtn) {
+    if (!clearBtn) return;
+
+    clearBtn.textContent = "CE";
+    clearBtn.classList.remove('clear');
+    clearBtn.classList.add('delete');
+    clearBtn.dataset['value'] = 'del';
+    clearBtn.dataset['key'] = 'Backspace';
+}
+
+function toggleDelBtn(delBtn) {
+    if (!delBtn) return;
+
+    delBtn.textContent = "AC";
+    delBtn.classList.remove('delete');
+    delBtn.classList.add('clear');
+    delBtn.dataset['value'] = 'clear';
+    delBtn.dataset['key'] = 'Escape';
+}
+
+function unaryOpAction(op, numstr) {
+    if (op === "negation") {
+        output.textContent = (numstr.indexOf('-') > -1) ? output.textContent.slice(1) : '-' + output.textContent;
+    } else if (op === "sqrt") {
+        output.textContent = round(Math.sqrt(output.textContent), 8);     // TODO handle neg sqrt error
+    } else if (op === "%") {
+        output.textContent = numstrToNumber(numstr) / 100;
+    }
+}
+
+function binaryOpAction(op, numstr) {
+    inputs.push(numstrToNumber(numstr));
+    inputs.push(op);
+    eq.textContent += `${numstr} ${op} `;
+    output.textContent = '';
 }
 
 function enableDecimalBtn() {
